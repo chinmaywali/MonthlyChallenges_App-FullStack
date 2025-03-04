9edc11a85515d8c90f7ab6b3536b5adb4495bf54
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2025-03-04 12:50:02 +0530

    Update ChallengeList.js

--- frontend/challenge-react/src/ChallengeList.js
+++ frontend/challenge-react/src/ChallengeList.js
@@ -1,26 +1,14 @@
-// import Challenge from './Challenge';
-
-// export default function ChallengeList({Challenges}){
-
-//     return(
-//         <div className="list-group">
-//             {Challenges.map(ch=>(
-//                 <div>
-//                     <Challenge key={ch.id} Challenges={ch}/>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// }
-
 import Challenge from './Challenge';
 
-export default function ChallengeList({ Challenges }) {
-  return (
-    <div className="list-group mt-3">
-      {Challenges.map(ch => (
-        <Challenge key={ch.id} Challenges={ch} />
-      ))}
-    </div>
+export default function ChallengeList({Challenges}){
+
+  return(
+      <div className="list-group">
+          {Challenges.map(ch=>(
+              <div>
+                  <Challenge key={ch.id} Challenges={ch}/>
+              </div>
+          ))}
+      </div>
   );
 }
